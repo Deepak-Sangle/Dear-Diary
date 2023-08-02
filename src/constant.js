88c35b906b8354e0a68c903672f04028a305224a
Deepak Sangle <sangleds20@iitk.ac.in>
2023-08-02 15:59:36 +0530

    added env var

--- src/constant.js
+++ src/constant.js
@@ -1,4 +1,8 @@
+
+const isDev = process.env.NODE_ENV === 'development' ;
+
+console.log(process.env.NODE_ENV)
+
 module.exports = {
-//  BASE_URI : "https://dear-diary-api.onrender.com" 
-  BASE_URI : "http://localhost:5000"
+ BASE_URI : isDev ? "https://dear-diary-api.onrender.com" : "http://localhost:5000"
 }
