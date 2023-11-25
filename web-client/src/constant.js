5e827280e14336b0a4495c0634655b02adfd65c2
Deepak Sangle <sangleds20@iitk.ac.in>
2023-11-25 07:53:12 +0530

    added env for react

--- web-client/src/constant.js
+++ web-client/src/constant.js
@@ -1,6 +1,7 @@
+const { REACT_APP_SERVER_URI, REACT_APP_LOCAL_ENV} = process.env
 
 const isDev = process.env.NODE_ENV === 'development' ;
 
 module.exports = {
- BASE_URI : !isDev ? "https://dear-diary-api.onrender.com" : "http://localhost:5000"
+ BASE_URI : !isDev ? REACT_APP_SERVER_URI : REACT_APP_LOCAL_ENV
 }
